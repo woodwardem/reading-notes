ffb16799f9f66d4f7d9733169f0fca56a33224c7
woodwardem <woodwardem@g.crookcountyschools.org>
2021-09-22 08:45:47 -0700

    fixed my reading note set up, worked more on arrow functions

--- code301/README.md
+++ code301/README.md
@@ -1,38 +1,7 @@
-<h3 align="center"><a href="./README.md">👈 Back to Table of Contents</a></h3>
+# 301 Reading Notes
+A collection of notes taken for Code Fellows 301: Intermediate Software Development.
 
----
+## Table Of Contents
 
-# ✍️ Notes
-What is ES6?
-ES6 is the more a enhabced version of javascript. It has more features like arrow functions, let and const. It technically is still javascript, but it is a more streamlined and enhanced version. 
-- When was ES6 released?
-ES6 was released in June of 2015.
-- List 5 new features of ES6.
-1. arrows- a new type of function
-2. classes
-3. let- a new way to define variables
-4. const- a new way to define variables
-5. modules
-- What are the new type of functions called that we will be using in 301?
-Arrow functions are the new type of function that we will be using in 301. 
-- What do the keywords `let` and `const` do?
-Let and const are keywords that can define variables and define functions. 
----
-
-## 📚 Resources 
-- [ES6 Features](http://es6-features.org/#Constants)
-- [W3 Schools: ES6](https://www.w3schools.com/js/js_es6.asp)
-- [Free Code Camp: ES6](https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/#es6)- When was ES6 released?
-
-
----
-
-## 📚 Resources 
-- [ES6 Features](http://es6-features.org/#Constants)
-- [W3 Schools: ES6](https://www.w3schools.com/js/js_es6.asp)
-- [Free Code Camp: ES6](https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/#es6)- Write your notes here...
-
----
-
-## 📚 Resources 
-- [New Features in JavaScript - ES6](http://es6-features.org/#Constants)
+- [ES6 Introduction](es6-intro.md)
+- [Arrow Functions](arrowFunctions)
--- code301/arrowFunctions/app.js
+++ code301/arrowFunctions/app.js
@@ -38,7 +38,7 @@ const withoutParens = course => {
 const oneLiner = course => `I cam currently enrolled in ${course}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('As a one-liner:', oneLiner('Code 301'));
+ console.log('As a one-liner:', oneLiner('Code 301'));
 
 
 // STEP 5
@@ -49,7 +49,7 @@ const add = function(num1, num2) {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('Let\'s do some math:', add(4, 5));
+ console.log('Let\'s do some math:', add(4, 5));
 
 
 // STEP 6
@@ -58,7 +58,7 @@ const add = function(num1, num2) {
 const addOneLiner = (num1, num2) => `${num1} + ${num2} = ${num1 + num2}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('Add as a one-liner:', addOneLiner(4, 5));
+console.log('Add as a one-liner:', addOneLiner(4, 5));
 
 
 // STEP 7
@@ -70,7 +70,7 @@ const multiLiner = word => {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('Multi-line arrow function:', multiLiner('hello'));
+ console.log('Multi-line arrow function:', multiLiner('hello'));
 
 
 // STEP 8
@@ -85,7 +85,7 @@ const oldObject = function(array) {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('Hello from the old object function', oldObject(['hi', 'hello', 'are you there?']));
+ console.log('Hello from the old object function', oldObject(['hi', 'hello', 'are you there?']));
 
 
 // STEP 9
@@ -98,7 +98,7 @@ const newObject = array => ({
 });
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('Hello from the new object function', newObject(['hi', 'hello', 'are you there?']));
+ console.log('Hello from the new object function', newObject(['hi', 'hello', 'are you there?']));
 
 
 // STEP 10
